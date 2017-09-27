1cf1aeb2c4b1b7631c983ee0560442502a5dbf8c
Chenmeng Wu <cwu@uscorp.centrillionbio.com>
2017-09-26 18:51:32 -0700

    9_26

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,29 @@
+import java.util.*;
 public class NBody {
+	public static double readRadius (String file){
+		In in = new In(file);
+		in.readInt();
+		double radius = in.readDouble();
+		return radius;
+	}
+
+	public static Planet[] readPlanets (String file){
+		ArrayList<Planet> lst = new ArrayList<Planet>();
+		In in = new In(file);
+		in.readInt();
+		in.readDouble();
+		int[] a = new int[4];
+		for(int element : a) {
+			double xxp = in.readDouble();
+			double yyp = in.readDouble();
+			double xxv = in.readDouble();
+			double yyv = in.readDouble();
+			double mm = in.readDouble();
+			String imageFile = in.readString();
+			lst.add(new Planet(xxp, yyp, xxv, yyv, mm, imageFile));
+		}
+		Planet[] lst2 = lst.toArray(new Planet[lst.size()]);
+		return lst2;
+	}
 
 }
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -24,4 +24,61 @@ public class Planet {
 			mass = p.mass;
 			imgFileName = p.imgFileName;
 	}
+
+	public double calcDistance(Planet p){
+			double distance = Math.sqrt(Math.pow(this.xxPos - p.xxPos, 2) + 
+			Math.pow(this.yyPos - p.yyPos, 2));
+			return distance;
+	}
+
+	public double calcForceExertedBy(Planet p){
+		double gravitationalG = 6.67 * Math.pow(10,-11);
+		double distance = this.calcDistance(p);
+		double force = gravitationalG * this.mass * p.mass / (distance * distance);
+		return force;
+	}
+
+	public double calcForceExertedByX(Planet p){
+		double xForce = this.calcForceExertedBy(p) * (p.xxPos - this.xxPos) / this.calcDistance(p);
+		return xForce;
+	}
+
+	public double calcForceExertedByY(Planet p){
+		double yForce = this.calcForceExertedBy(p) * (p.yyPos - this.yyPos) / this.calcDistance(p);
+		return yForce;
+	}
+
+	public double calcNetForceExertedByX (Planet[] parray){
+		double netForceX = 0;
+		for (int i = 0; i < parray.length; i++){
+		Planet p = parray[i];
+			if (! this.equals(p)){
+				netForceX += this.calcForceExertedByX(p);
+			}
+		}
+		return netForceX;
+	}
+
+	public double calcNetForceExertedByY (Planet[] parray){
+		double netForceY = 0;
+		for (int i = 0; i < parray.length; i++){
+		Planet p = parray[i];
+			if (! this.equals(p)){
+				netForceY += this.calcForceExertedByY(p);
+			}
+		}
+		return netForceY;
+	}
+
+	public void update (double t, double fx, double fy){
+		double ax = fx / this.mass;
+		double ay = fy / this.mass;
+		this.xxVel += ax * t;
+		this.yyVel += ay * t;
+		this.xxPos += this.xxVel * t;
+		this.yyPos += this.yyVel * t;
+	}
 }
+
+
+
