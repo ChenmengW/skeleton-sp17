f8c718260960e1c8d9eb26f5cb4fc385ed53f9eb
ChenmengW <wuchenmeng@gmail.com>
2017-10-26 00:09:42 -0700

    create_column_to_be_tested

--- proj2/db/Column.java
+++ proj2/db/Column.java
@@ -1,4 +1,11 @@
 package db;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Column {
+    String name;
+    String type;
+    List<String> elements = new ArrayList<String>();
 }
--- proj2/db/Table.java
+++ proj2/db/Table.java
@@ -2,15 +2,24 @@ package db;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Table {
     private String name;
-    private List <Column> columns = new ArrayList<Column>();
+    private HashMap <String, List> chm = new HashMap<String, List>();
+
+    private static HashMap<String, List> makeHashMap (List<Column> columns){
+        HashMap <String, List> chm = new HashMap<String, List>();
+        for(Column column : columns){
+            chm.put(column.name, column.elements);
+        }
+        return chm;
+    }
 
 
-    public Table (String name, List columns){
+    public Table (String name, List<Column> columns){
         this.name = name;
-        this.columns = columns;
+        chm = makeHashMap(columns);
     }
 
     public void insertIntoTable (String name, Column column) {
