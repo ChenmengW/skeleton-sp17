66b0099ba1756f7ad697ae6303dcf5aaefdb3a09
Chenmeng Wu <wuchenmeng@gmail.com>
2017-11-03 19:18:58 -0700

    join

--- proj2/db/Table.java
+++ proj2/db/Table.java
@@ -1,31 +1,81 @@
 package db;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Table {
     private String name;
-    private List <Column> columns = new ArrayList<Column>();
 
+    List <String> columns = new ArrayList();
+    private List <String> rows = new ArrayList();
 
-    public Table (String name, List columns){
+    /* Row first */
+    private LinkedHashMap<String,LinkedHashMap<String,String>> data = new LinkedHashMap();
+    /*
+    private HashMap<String,String> columnType = new HashMap();
+    */
+
+    public Table (){}
+
+    public Table (String name, List columns, HashMap columnType){
         this.name = name;
+
         this.columns = columns;
+
+        /*
+        this.columnType = columnType;
+        */
     }
 
-    public void insertIntoTable (String name, Column column) {
-        /*write the table*/
-        InsertIntoTable.conduct(name, column);
+    public String insertIntoTable (List<String> rowData) {
+        String firstRowData = rowData.get(0);
+        for (int i = 0; i < getColumnNumber(); i ++) {
+            LinkedHashMap<String, String> tempElement = new LinkedHashMap();
+            tempElement.put(getColumnName(i), rowData.get(i));
+            data.put(firstRowData, tempElement);
+        }
+        return "";
     }
 
-    public void printTable () {
-        /*write the table*/
-        PrintTable.conduct();
+    public String printTable () {
+        for (Map.Entry<String,LinkedHashMap<String, String>> entry: data.entrySet()){
+            String row ="";
+            for (Map.Entry<String,String> entry2: entry.getValue().entrySet()) {
+                row +=  entry.getKey() + ",";
+            }
+            row = row.substring(0,row.length() - 1);
+            System.out.println(row);
+        }
+        return "";
+    }
+
+
+    private Table join(Table table){
+        Join j = new Join(this,table);
+        return j.conduct();
     }
 
     public void selectTable (String name) {
         /*write the table*/
         SelectTable.conduct(name);
     }
+
+    private String getColumnName(int i){
+        return columns.get(i);
+    }
+
+    private int getColumnNumber(){
+        return columns.size();
+    }
+
+    /*
+    private void makeColumnsList(){
+        List<String> result = new ArrayList<String>();
+        for (Map.Entry<String,?> entry: data.entrySet()){
+            result.add(entry.getKey());
+        }
+        columns = result;
+    }
+    */
+
 
 }
