2118377c0b1d1ae80595bf7b8d6aec2506819cf7
Chenmeng Wu <wuchenmeng@gmail.com>
2017-11-21 18:12:46 -0800

    hw3halfdone

--- hw3/hw3/puzzle/Solver.java
+++ hw3/hw3/puzzle/Solver.java
@@ -1,14 +1,15 @@
 package hw3.puzzle;
-
-
-import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Solver {
     Node initialNode;
     MinPQ<Node> pq = new MinPQ(5);
+    List<WorldState> nodelist= new ArrayList();
 
     public Solver(WorldState initial){
-        initialNode = new Node(initial);
+        initialNode = new Node(initial, 0, null);
     }
 
     public int moves(){
@@ -17,16 +18,53 @@ public class Solver {
 
     public Iterable<WorldState> solution(){
         pq.insert(initialNode);
+        while ( ! pq.min().worldState.isGoal()) {
+            Node minNode = pq.delMin();
+            nodelist.add(minNode.worldState);
+            for (WorldState w : minNode.worldState.neighbors()) {
+                Node neighborNode = new Node(w, minNode.moves+1, minNode);
+                if (minNode.pre == null || ! neighborNode.worldState.equals(minNode.pre.worldState)){
+                    pq.insert(neighborNode);
+                }
+            }
+        }
+        nodelist.add(pq.delMin().worldState);
+        return nodelist;
     }
 
 
-
-    private class Node{
+    private class Node implements Comparable<Node>{
         WorldState worldState;
-        int moves = 0;
+        Integer moves = 0;
         Node pre;
-        Node(WorldState worldState){
+
+        Node(WorldState worldState, int move, Node pre){
             this.worldState = worldState;
+            this.moves = move;
+            this.pre = pre;
         }
+
+        private Integer dis(){
+            return moves + worldState.estimatedDistanceToGoal();
+        }
+
+        @Override
+        public int compareTo(Node node){
+             return this.dis() - node.dis();
+        }
+
+        /*
+        @Override
+        public boolean equals(Object node1){
+            Node node = (Node) node1;
+            return this.worldState.equals(node.worldState);
+        }
+
+
+        @Override
+        public int hashCode(){
+            return pri().hashCode();
+        }
+        */
     }
 }
