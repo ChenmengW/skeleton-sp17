8e6453637465bc886a81e1387cc81c5171ebbee7
Chenmeng Wu <wuchenmeng@gmail.com>
2017-11-28 17:18:38 -0800

    lab12_quicksort

--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -47,13 +47,49 @@ public class QuickSort {
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
-        // Your code here!
+        for(Item item:unsorted){
+            if(item.equals(pivot)){
+                equal.enqueue(pivot);
+            }
+            else if(item.compareTo(pivot)<0){
+                less.enqueue(item);
+            }
+            else{greater.enqueue(item);}
+        }
+
+
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> quickSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+        if (items.isEmpty()){
+            Queue<Item> result = new Queue<Item>();
+            return result;
+        }
+        Item pivot = getRandomItem(items);
+        Queue<Item> less = new Queue<Item>();
+        Queue<Item> equal = new Queue<Item>();
+        Queue<Item> greater = new Queue<Item>();
+        partition(items, pivot, less, equal, greater);
+        less = quickSort(less);
+        greater = quickSort(greater);
+        Queue<Item> result = catenate(less,equal);
+        result = catenate(result,greater);
+        return result;
     }
+
+
+
+    public static void main (String[] args){
+        Queue<String> students = new Queue<String>();
+        students.enqueue("Alice");
+        students.enqueue("Vanessa");
+        students.enqueue("Ethan");
+        System.out.print(students.toString());
+        Queue<String> sortedQueue = quickSort(students);
+        System.out.print(students.toString());
+        System.out.print(sortedQueue.toString());
+    }
+
 }
